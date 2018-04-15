4eb339d2ffb9df54a1defa05063a888431973266
Rui Fartaria <rui.fartaria@gmail.com>
2018-04-15 12:58:41 +0100

    change to EN

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-if diff <(sort $1) <(sort $2) &> /dev/null; then echo IGUAIS; else echo DIFERENTES; fi
+if diff <(sort $1) <(sort $2) &> /dev/null; then echo EQUAL; else echo DIFFERENT; fi
 
