b9cbbc5689f2e2e0aedaf49b93bc74921573b23d
Rui Fartaria <rui.fartaria@gmail.com>
2018-05-15 17:57:55 +0100

    on our way to phase 4

--- src/file.c
+++ src/file.c
@@ -33,95 +33,182 @@ struct file Ficheiros; // informação sobre nomes e handles de ficheiros
 
 void file_begin(char *fic_in, char *fic_out, char *fic_log) {
 	//==============================================
-	// GUARDAR NOMES DOS FICHEIROS NA ESTRUTURA Ficheiros
+    // SAVE FILE NAMES IN STRUCT Ficheiros
 	//
-	so_file_begin(fic_in, fic_out, fic_log);
+	//so_file_begin(fic_in, fic_out, fic_log);
+    Ficheiros.entrada = (char *) calloc(1, sizeof(fic_in)+1);
+    strcpy(Ficheiros.entrada, fic_in);
+
+	Ficheiros.saida=(char *) calloc(1,sizeof(fic_out)+1);
+	strcpy(Ficheiros.saida, fic_out);
+
+	Ficheiros.log=(char *) calloc(1,sizeof(fic_log)+1);
+	strcpy(Ficheiros.log, fic_log);
 	//==============================================
 
 	//==============================================
-	// ABRIR FICHEIRO DE ENTRADA
-	// em modo de leitura
-	so_file_open_file_in();
+    // OPEN INPUT FILE
+	// read mode
+    // so_file_open_file_in();
+    Ficheiros.h_in = fopen(Ficheiros.entrada,"r");
 	//==============================================
 
-	// parse do ficheiro de teste
-	// esta funcao prepara os campos da estrutura Config (char *)
-	// com os dados do ficheiro de entrada
+    // parse test file
+    // this function prepares the fields of struct Config (char*)
+	// with data from input file
 	if (ini_parse_file(Ficheiros.h_in, handler, &Config) < 0) {
 		printf("Erro a carregar o ficheiro de teste'\n");
 		exit(1);
 	}
 
-	// agora e' preciso inicializar os restantes campos da estrutura Config
+    // now we need to initialize the remaining fields in struct Config
 
 	//==============================================
-	// CONTAR CURRENCIES
-	// usar strtok para percorrer Config.list_currencies
-	// guardar resultado em Config.CURRENCIES
-	so_file_count_currencies();
+	// COUNT CURRENCIES
+    // use strtok to iterate Config.list_currencies
+    // save the result in Config.CURRENCIES
+	// so_file_count_currencies();
+    //printf("%s", Config.list_currencies); //exit(0);
+    char tmpstr[1000] = {0};
+	strcpy(tmpstr,Config.list_currencies);
+    char *cch;
+    cch = strtok(tmpstr," ");
+	Config.CURRENCIES=0;
+    while (cch != NULL)
+    {
+		Config.CURRENCIES++;
+		cch = strtok (NULL, " ");
+    }
+    //printf("\n%d\n", Config.CURRENCIES); exit(0);
 	//==============================================
 
-	// iniciar memoria para o vetor com o stock por CURRENCY e semaforo
+    // initialize memory for vector containing the stock for CURRENCY and semaphore
 	memory_create_stock();
 	prodcons_create_stock();
 
 	//==============================================
-	// LER STOCK DE CADO CURRENCY
-	// percorrer Config.list_currencies e
-	// guardar cada valor no vetor Config.stock
-	so_file_read_stock();
+    // READ STOCK OF EACH CURRENCY
+    // iterate Config.list_currencies and
+    // save each value in the array Config.stock
+    // so_file_read_stock();
+    //printf("\nstr = %s\n", Config.list_currencies);
+    strcpy(tmpstr,Config.list_currencies);
+	cch = strtok(tmpstr," ");
+	int *istock = Config.stock;
+    int count = 0;
+    while (cch != NULL)
+    {
+		sscanf(cch, "%d", istock);
+		cch = strtok (NULL, " ");
+		istock++;
+        //printf("\n%d\n", Config.stock[count]);
+        count++;
+    }
+    //exit(0);
 	//==============================================
 
 	//==============================================
-	// CONTAR INVESTORS
-	// usar strtok para percorrer Config.list_investors
-	// guardar resultado em Config.INVESTORS
+	// COUNT INVESTORS
+    // use strtok to iterate Config.list_investors
+	// keep the result in  Config.INVESTORS
 	so_file_count_investors();
+//    printf("\n%s\n", Config.list_investors); //exit(0);
+//    strcpy(tmpstr,Config.list_investors);
+//    cch = strtok(tmpstr," ");
+//	Config.INVESTORS = 0;
+//    while (cch != NULL)
+//    {
+//		Config.INVESTORS++;
+//        cch = strtok (NULL, " ");
+//    }
+//    printf("\n%d\n", Config.INVESTORS); //exit(0);
 	//==============================================
 
 	//==============================================
-	// CONTAR BROKERS
-	// usar strtok para percorrer Config.list_brokers
-	// guardar resultado em Config.BROKERS
-	so_file_count_brokers();
+	// COUNT BROKERS
+    // use strtok to iterate Config.list_brokers
+	// keep the result in Config.BROKERS
+	//so_file_count_brokers();
+    //printf("\n%s\n", Config.list_brokers); exit(0);
+    strcpy(tmpstr,Config.list_brokers);
+    cch = strtok(tmpstr," ");
+	Config.BROKERS = 0;
+    while (cch != NULL)
+    {
+		Config.BROKERS++;
+        cch = strtok (NULL, " ");
+    }
 	//==============================================
 
 	//==============================================
-	// CONTAR EXCHANGES
+	// COUNT EXCHANGES
 	// usar strtok para percorrer Config.list_exchanges
 	// guardar resultado em Config.EXCHANGES
 	so_file_count_exchanges();
+    //printf("\n%s\n", Config.list_exchanges); exit(0);
+    strcpy(tmpstr,Config.list_exchanges);
+    cch = strtok(tmpstr,",");
+	Config.EXCHANGES = 0;
+    while (cch != NULL)
+    {
+		Config.EXCHANGES++;
+        cch = strtok (NULL, ",");
+    }
 	//==============================================
 
 	so_file_read_currencies();
 
 	//==============================================
-	// LER CAPACITYS DOS BUFFERS
-	// usar strtok para percorrer Config.list_buffers
-	// guardar primeiro tamanho em Config.BUFFER_REQUEST
-	// guardar segundo tamanho em Config.BUFFER_ORDER
-	// guardar terceiro tamanho em Config.BUFFER_RECEIPT
-	so_file_read_capacities();
+    // READ CAPACITIES OF BUFFERS
+    // use strtok to iterate Config.list_fuffers
+    // save first size in Config.BUFFER_REQUEST
+    // save second size in Config.BUFFER_ORDER
+    // save third size in Config.BUFFER_RECEIPT
+	//so_file_read_capacities();
+    strcpy(tmpstr,Config.list_buffers);
+    cch = strtok(tmpstr," ");
+    sscanf(cch, "%d", &Config.BUFFER_REQUEST);
+    
+    cch = strtok(NULL," ");
+    sscanf(cch, "%d", &Config.BUFFER_ORDER);
+    
+    cch = strtok(NULL," ");
+    sscanf(cch, "%d", &Config.BUFFER_RECEIPT);	
 	//==============================================
 
 	//==============================================
-	// ABRIR FICHEIRO DE SAIDA (se foi especificado)
-	// em modo de escrita
-	so_file_open_file_out();
+    // OPEN OUTPUT FILE (if given)
+    // write mode
+	// so_file_open_file_out();
+    if (Ficheiros.saida != NULL)
+    {
+        Ficheiros.h_out = fopen(Ficheiros.saida,"w");
+    }
 	//==============================================
 
 	//==============================================
 	// ABRIR FICHEIRO DE LOG (se foi especificado)
-	// em modo de escrita
-	so_file_open_file_log();
+    // OPEN LOG FILE (if given)
+	// write mode
+	// so_file_open_file_log();
+    if (Ficheiros.log != NULL)
+    {
+        Ficheiros.h_log = fopen(Ficheiros.log,"w");
+    }
 	//==============================================
 }
 
 void file_destroy() {
 	//==============================================
-	// LIBERTAR ZONAS DE MEMÓRIA RESERVADAS DINAMICAMENTE
-	// que podem ser: Ficheiros.entrada, Ficheiros.saida, Ficheiros.log
-	so_file_destroy();
+    // FREE DYNAMICALLY ALLOCATED MEMORY
+    // those may be: Ficheiros.entrada, Ficheiros.saida, Ficheiros.log
+	// so_file_destroy();
+    free(Ficheiros.entrada);
+    free(Ficheiros.saida);
+    free(Ficheiros.log);
+    if (ftell(Ficheiros.h_in) >= 0) fclose(Ficheiros.h_in);
+    if (ftell(Ficheiros.h_out) >= 0) fclose(Ficheiros.h_out);
+    if (ftell(Ficheiros.h_log) >= 0) fclose(Ficheiros.h_log);
 	//==============================================
 }
 
@@ -136,9 +223,9 @@ void file_write_log_file(int etapa, int id) {
 		t_diff = time_untilnow();
 
 		//==============================================
-		// ESCREVER DADOS NO FICHEIRO DE LOG
+        // WRITE DATA IN LOG FILE
 		//
-		// o log deve seguir escrupulosamente o formato definido
+        // the log must scrupulously follow the defined format
 		so_file_write_log_file(etapa, id, t_diff);
 		//==============================================
 
@@ -150,7 +237,8 @@ void file_write_line(char * linha) {
 	//==============================================
 	// ESCREVER UMA LINHA NO FICHEIRO DE SAIDA
 	//
-	so_file_write_line(linha);
+	// so_file_write_line(linha);
+    fprintf(Ficheiros.h_out, "%s", linha);
 	//==============================================
 }
 
--- src/prodcons.c
+++ src/prodcons.c
@@ -34,19 +34,7 @@ sem_t * semaphore_create(char * name, int value) {
 	//==============================================
 	// FUNÇÃO GENÉRICA DE CRIAÇÃO DE UM SEMÁFORO
 	//
-	// return so_semaphore_create(name, value);
-    char name_uid[100] = {0};
-    char error_msg[1000] = {0};
-    sprintf(name_uid,"/%s_%d", name, getuid());
-    
-    sem_t* semaphore = sem_open(name_uid, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, value);
-	if (semaphore == SEM_FAILED)
-	{
-            sprintf(error_msg, "semaphore_create : %s", name_uid);
-			perror(error_msg);
-			exit(16);
-	}
-	return semaphore;
+	return so_semaphore_create(name, value);
 	//==============================================
 }
 
@@ -55,8 +43,7 @@ void prodcons_create_stock() {
 	// CRIAR MUTEX PARA CONTROLAR O ACESSO AO STOCK
 	//
 	// utilizar a função genérica semaphore_create
-	// so_prodcons_create_stock();
-    ProdCons.stock_mutex = semaphore_create(STR_SEM_STOCK_MUTEX, 1);
+	so_prodcons_create_stock();
 	//==============================================
 }
 
@@ -65,18 +52,7 @@ void prodcons_create_buffers() {
 	// CRIAR SEMAFOROS PARA CONTROLAR O ACESSO AOS 3 BUFFERS
 	//
 	// utilizar a função genérica semaphore_create
-	// so_prodcons_create_buffers();
-    ProdCons.request_p_full = semaphore_create(STR_SEM_REQUEST_FULL, 0);
-	ProdCons.request_p_empty = semaphore_create(STR_SEM_REQUEST_EMPTY, Config.BUFFER_REQUEST);
-	ProdCons.request_p_mutex = semaphore_create(STR_SEM_REQUEST_MUTEX, 1);
-	
-	ProdCons.request_e_full = semaphore_create(STR_SEM_ORDER_FULL, 0);
-	ProdCons.request_e_empty = semaphore_create(STR_SEM_ORDER_EMPTY, Config.BUFFER_ORDER);
-	ProdCons.request_e_mutex = semaphore_create(STR_SEM_ORDER_MUTEX, 1);
-	
-	ProdCons.receipts_r_full = semaphore_create(STR_SEM_RECEIPT_FULL, 0);
-	ProdCons.receipts_r_empty = semaphore_create(STR_SEM_RECEIPT_EMPTY, Config.BUFFER_RECEIPT);
-	ProdCons.receipts_r_mutex = semaphore_create(STR_SEM_RECEIPT_MUTEX, 1);
+	so_prodcons_create_buffers();
 	//==============================================
 }
 
@@ -84,25 +60,7 @@ void semaphore_destroy(char * name, void * ptr) {
 	//==============================================
 	// FUNÇÃO GENÉRICA DE DESTRUIÇÃO DE UM SEMÁFORO
 	//
-	//so_semaphore_destroy(name, ptr);
-    char name_uid[100] = {0};
-    sprintf(name_uid,"/%s_%d", name, getuid());
-	
-	char error_msg[100] = {0};
-	
-	if (sem_close(ptr) == -1)
-	{
-		sprintf(error_msg, "semaphore_destroy sem_close: %s", name_uid);
-		perror(error_msg);
-		exit(17);
-	}
-	
-	if (sem_unlink(name_uid) == -1) 
-	{
-		sprintf(error_msg, "semaphore_destroy sem_unlink: %s", name_uid);
-		perror(error_msg);
-		exit(18);
-	}
+	so_semaphore_destroy(name, ptr);
 	//==============================================
 }
 
@@ -111,20 +69,7 @@ void prodcons_destroy() {
 	// DESTRUIR SEMÁFORO E RESPETIVO NOME
 	//
 	// utilizar a função genérica semaphore_destroy
-	//so_prodcons_destroy();
-    semaphore_destroy(STR_SEM_STOCK_MUTEX, ProdCons.stock_mutex);
-	
-	semaphore_destroy(STR_SEM_REQUEST_FULL, ProdCons.request_p_full);
-	semaphore_destroy(STR_SEM_REQUEST_EMPTY, ProdCons.request_p_empty);
-	semaphore_destroy(STR_SEM_REQUEST_MUTEX, ProdCons.request_p_mutex);
-	
-	semaphore_destroy(STR_SEM_ORDER_FULL, ProdCons.request_e_full);
-	semaphore_destroy(STR_SEM_ORDER_EMPTY, ProdCons.request_e_empty);
-	semaphore_destroy(STR_SEM_ORDER_MUTEX, ProdCons.request_e_mutex);
-	
-	semaphore_destroy(STR_SEM_RECEIPT_FULL, ProdCons.receipts_r_full);
-	semaphore_destroy(STR_SEM_RECEIPT_EMPTY, ProdCons.receipts_r_empty);
-	semaphore_destroy(STR_SEM_RECEIPT_MUTEX, ProdCons.receipts_r_mutex);
+	so_prodcons_destroy();
 	//==============================================
 }
 
@@ -133,18 +78,7 @@ void prodcons_request_p_produce_begin() {
 	//==============================================
 	// CONTROLAR ACESSO DE CURRENCYR AO BUFFER PEDIDO DE CURRENCY
 	//
-	//so_prodcons_request_p_produce_begin();
-    if (sem_wait(ProdCons.request_p_empty) == -1)
-	{
-		perror("sem_wait : ProdCons.request_p_empty");
-		exit(20);
-	}
-	
-	if (sem_wait(ProdCons.request_p_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.request_p_mutex");
-		exit(20);
-	}
+	so_prodcons_request_p_produce_begin();
 	//==============================================
 }
 
@@ -153,18 +87,7 @@ void prodcons_request_p_produce_end() {
 	//==============================================
 	// CONTROLAR ACESSO DE CURRENCYR AO BUFFER PEDIDO DE CURRENCY
 	//
-	//so_prodcons_request_p_produce_end();
-    if (sem_post(ProdCons.request_p_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.request_p_mutex");
-		exit(20);
-	}
-	
-	if (sem_post(ProdCons.request_p_full) == -1)
-	{
-		perror("sem_post : ProdCons.request_p_full");
-		exit(20);
-	}
+	so_prodcons_request_p_produce_end();
 	//==============================================
 }
 
@@ -173,18 +96,7 @@ void prodcons_request_p_consume_begin() {
 	//==============================================
 	// CONTROLAR ACESSO DE CONSUMIDOR AO BUFFER PEDIDO DE CURRENCY
 	//
-	//so_prodcons_request_p_consume_begin();
-    if (sem_wait(ProdCons.request_p_full) == -1)
-	{
-		perror("sem_wait : ProdCons.request_p_full");
-		exit(20);
-	}
-	
-	if (sem_wait(ProdCons.request_p_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.request_p_mutex");
-		exit(20);
-	}
+	so_prodcons_request_p_consume_begin();
 	//==============================================
 }
 
@@ -193,18 +105,7 @@ void prodcons_request_p_consume_end() {
 	//==============================================
 	// CONTROLAR ACESSO DE CONSUMIDOR AO BUFFER PEDIDO DE CURRENCY
 	//
-	//so_prodcons_request_p_consume_end();
-    if (sem_post(ProdCons.request_p_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.request_p_mutex");
-		exit(20);
-	}
-	
-	if (sem_post(ProdCons.request_p_empty) == -1)
-	{
-		perror("sem_post : ProdCons.request_p_empty");
-		exit(20);
-	}
+	so_prodcons_request_p_consume_end();
 	//==============================================
 }
 
@@ -213,18 +114,7 @@ void prodcons_request_e_produce_begin() {
 	//==============================================
 	// CONTROLAR ACESSO DE CURRENCYR AO BUFFER PEDIDO DE ENCOMENDA
 	//
-	//so_prodcons_request_e_produce_begin();
-    if (sem_wait(ProdCons.request_e_empty) == -1)
-	{
-		perror("sem_wait : ProdCons.request_e_empty");
-		exit(20);
-	}
-	
-	if (sem_wait(ProdCons.request_e_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.request_e_mutex");
-		exit(20);
-	}
+	so_prodcons_request_e_produce_begin();
 	//==============================================
 }
 
@@ -233,18 +123,7 @@ void prodcons_request_e_produce_end() {
 	//==============================================
 	// CONTROLAR ACESSO DE CURRENCYR AO BUFFER PEDIDO DE ENCOMENDA
 	//
-	//so_prodcons_request_e_produce_end();
-    if (sem_post(ProdCons.request_e_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.request_e_mutex");
-		exit(20);
-	}
-	
-	if (sem_post(ProdCons.request_e_full) == -1)
-	{
-		perror("sem_post : ProdCons.request_e_full");
-		exit(20);
-	}
+	so_prodcons_request_e_produce_end();
 	//==============================================
 }
 
@@ -253,18 +132,7 @@ void prodcons_request_e_consume_begin() {
 	//==============================================
 	// CONTROLAR ACESSO DE CONSUMIDOR AO BUFFER PEDIDO DE ENCOMENDA
 	//
-	//so_prodcons_request_e_consume_begin();
-    if (sem_wait(ProdCons.request_e_full) == -1)
-	{
-		perror("sem_wait : ProdCons.request_e_full");
-		exit(20);
-	}
-	
-	if (sem_wait(ProdCons.request_e_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.request_e_mutex");
-		exit(20);
-	}
+	so_prodcons_request_e_consume_begin();
 	//==============================================
 }
 
@@ -273,18 +141,7 @@ void prodcons_request_e_consume_end() {
 	//==============================================
 	// CONTROLAR ACESSO DE CONSUMIDOR AO BUFFER PEDIDO DE ENCOMENDA
 	//
-	// so_prodcons_request_e_consume_end();
-    if (sem_post(ProdCons.request_e_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.request_e_mutex");
-		exit(20);
-	}
-	
-	if (sem_post(ProdCons.request_e_empty) == -1)
-	{
-		perror("sem_post : ProdCons.request_e_empty");
-		exit(20);
-	}
+	so_prodcons_request_e_consume_end();
 	//==============================================
 }
 
@@ -293,18 +150,7 @@ void prodcons_receipt_r_produce_begin() {
 	//==============================================
 	// CONTROLAR ACESSO DE CURRENCYR AO BUFFER RECIBO
 	//
-	// so_prodcons_receipt_r_produce_begin();
-    if (sem_wait(ProdCons.receipts_r_empty) == -1)
-	{
-		perror("sem_wait : ProdCons.receipts_r_empty");
-		exit(20);
-	}
-	
-	if (sem_wait(ProdCons.receipts_r_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.receipts_r_mutex");
-		exit(20);
-	}
+	so_prodcons_receipt_r_produce_begin();
 	//==============================================
 }
 
@@ -313,18 +159,7 @@ void prodcons_receipt_r_produce_end() {
 	//==============================================
 	// CONTROLAR ACESSO DE CURRENCYR AO BUFFER RECIBO
 	//
-	//so_prodcons_receipt_r_produce_end();
-    if (sem_post(ProdCons.receipts_r_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.receipts_r_mutex");
-		exit(20);
-	}
-	
-	if (sem_post(ProdCons.receipts_r_full) == -1)
-	{
-		perror("sem_post : ProdCons.receipts_r_full");
-		exit(20);
-	}
+	so_prodcons_receipt_r_produce_end();
 	//==============================================
 }
 
@@ -333,18 +168,7 @@ void prodcons_receipt_r_consume_begin() {
 	//==============================================
 	// CONTROLAR ACESSO DE CONSUMIDOR AO BUFFER RECIBO
 	//
-	// so_prodcons_receipt_r_consume_begin();
-    if (sem_wait(ProdCons.receipts_r_full) == -1)
-	{
-		perror("sem_wait : ProdCons.receipts_r_full");
-		exit(20);
-	}
-	
-	if (sem_wait(ProdCons.receipts_r_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.receipts_r_mutex");
-		exit(20);
-	}
+	so_prodcons_receipt_r_consume_begin();
 	//==============================================
 }
 
@@ -353,18 +177,7 @@ void prodcons_receipt_r_consume_end() {
 	//==============================================
 	// CONTROLAR ACESSO DE CONSUMIDOR AO BUFFER RECIBO
 	//
-	//so_prodcons_receipt_r_consume_end();
-    if (sem_post(ProdCons.receipts_r_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.receipts_r_mutex");
-		exit(20);
-	}
-	
-	if (sem_post(ProdCons.receipts_r_empty) == -1)
-	{
-		perror("sem_post : ProdCons.receipts_r_empty");
-		exit(20);
-	}
+	so_prodcons_receipt_r_consume_end();
 	//==============================================
 }
 
@@ -373,22 +186,7 @@ void prodcons_buffers_begin() {
 	//==============================================
 	// GARANTIR EXCLUSÃO MÚTUA NO ACESSO AOS 3 BUFFERS
 	//
-	// so_prodcons_buffers_begin();
-    if (sem_wait(ProdCons.request_p_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.request_p_mutex");
-		exit(20);
-	}
-	if (sem_wait(ProdCons.request_e_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.request_e_mutex");
-		exit(20);
-	}
-	if (sem_wait(ProdCons.receipts_r_mutex) == -1)
-	{
-		perror("sem_wait : ProdCons.receipts_r_mutex");
-		exit(20);
-	}
+	so_prodcons_buffers_begin();
 	//==============================================
 }
 
@@ -397,22 +195,7 @@ void prodcons_buffers_end() {
 	//==============================================
 	// FIM DA ZONA DE EXCLUSÃO MÚTUA NO ACESSO AOS 3 BUFFERS
 	//
-	// so_prodcons_buffers_end();
-    if (sem_post(ProdCons.request_p_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.request_p_mutex");
-		exit(20);
-	}
-	if (sem_post(ProdCons.request_e_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.request_e_mutex");
-		exit(20);
-	}
-	if (sem_post(ProdCons.receipts_r_mutex) == -1)
-	{
-		perror("sem_post : ProdCons.receipts_r_mutex");
-		exit(20);
-	}
+	so_prodcons_buffers_end();
 	//==============================================
 }
 
@@ -424,25 +207,6 @@ int prodcons_update_stock(int currency) {
 	// se stock de currency>0 então decrementá-lo de uma unidade e 
 	//   função devolve 1
 	// se stock de currency=0 então função devolve 0
-	//return so_prodcons_update_stock(currency);
-    int res=0;
-       
-    if(sem_wait (ProdCons.stock_mutex)==-1){
-        perror("sem_wait : ProdCons.stock_mutex");
-        exit(20);
-    }
-    
-    if(Config.stock[currency]>0){
-        Config.stock[currency]--;
-        res=1;
-    }
-    
-    
-    if(sem_post (ProdCons.stock_mutex)==-1){
-        perror("sem_post : ProdCons.stock_mutex");
-        exit(20);
-    }
-	return res;
+	return so_prodcons_update_stock(currency);
 	//==============================================
 }
-
